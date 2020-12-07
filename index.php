9fcd1da7ea2ce9649ea29640f3e4a24519be674b
KAENG <yamato@KAENGs-MacBook-Pro.local>
2020-12-07 15:49:59 +0700

    update index.php

--- index.php
+++ index.php
@@ -23,10 +23,17 @@ function onSignIn(googleUser) {
   console.log("ID Token: " + id_token);
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 
 </script>
 </head>
 <body>
     <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <a href="#" onclick="signOut();">Sign out</a>
 </body>
 </html>
