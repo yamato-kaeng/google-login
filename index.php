38f3a982bff7a952c820449552dddd243555bdbb
KAENG <yamato@KAENGs-MacBook-Pro.local>
2020-12-07 15:48:24 +0700

    update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <script>
 
 function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
   console.log("ID: " + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
