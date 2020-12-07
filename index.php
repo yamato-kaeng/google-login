0c766edbadd94b720cf85efe7a14993543e1b2f7
KAENG <yamato@KAENGs-MacBook-Pro.local>
2020-12-07 15:38:46 +0700

    init file php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8">
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="659694983275-e2mu1cfbo61prcbamtketkcqhhsr9f5p.apps.googleusercontent.com">
+
+<script>
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+
+</script>
+</head>
+<body>
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+</body>
+</html>
